89ec62fc3c37eb9670f923c185d214cb720b5f17
Mohamed Ayman <mrmezax09@gmail.com>
2026-02-28 17:03:34 +0200

    Add API call to retrieve all users

--- client/src/apiCalls/users.js
+++ client/src/apiCalls/users.js
@@ -9,4 +9,13 @@ const getLoggedUser = async () => {
   }
 };
 
-export default getLoggedUser; 
+export const getAllUsers = async () => {
+  try {
+    const response = await axiosInstance.get("/api/user/get-all-users");
+    return response.data;
+  } catch (error) {
+    return error;
+  }
+};
+
+export default getLoggedUser;
